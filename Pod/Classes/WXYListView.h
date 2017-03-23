549dc0b5461f7a24702de51c4996895f8d038d8e
liulian <liulian@syswin.com>
2017-03-23 17:33:23 +0800

    init proj

---
+++ Pod/Classes/WXYListView.h
@@ -0,0 +1,13 @@
+//
+//  WXYListView.h
+//  Pods
+//
+//  Created by LiuLian on 23/03/2017.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WXYListView : UIView
+
+@end
---
+++ Pod/Classes/WXYListView.m
@@ -0,0 +1,21 @@
+//
+//  WXYListView.m
+//  Pods
+//
+//  Created by LiuLian on 23/03/2017.
+//
+//
+
+#import "WXYListView.h"
+
+@implementation WXYListView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
